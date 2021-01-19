a6be81e8bfb4db348f885692690b36046c245dc3
VillalobosNicole <nicolevillalobosbe@gmail.com>
2021-01-19 03:10:55 -0500

    subiendo html, index y comentando validator

--- src/index.js
+++ src/index.js
@@ -1,3 +1,86 @@
-import validator from './validator.js';
+ //import validator from './validator.js';
+ 
+//VISUALIZACION DE PÁGINAS 
+document.getElementById("firstPage").style.display = "block";
+document.getElementById("card").style.display = "block";
+document.getElementById("secondPage").style.display = "none";
+document.getElementById("footerProject").style.display = "none";
 
-console.log(validator);
+
+//QUE SE MUESTRE SOLO LA SEGUNDA PÁGINA
+// eslint-disable-next-line no-unused-vars
+document.getElementById("btnValidate").addEventListener('click', function(){
+    document.getElementById("secondPage").style.display = "block";
+    document.getElementById("firstPage").style.display = "none";
+    document.getElementById("card").style.display = "none";
+    document.getElementById("footerProject").style.display = "block";
+
+});
+
+const reload = document.getElementById('reload');
+reload.addEventListener('click', function(){ 
+    location.reload();
+});
+
+const showNumberInCard = document.querySelector(" .number");
+const showNameInCard = document.querySelector(" .titularName");
+
+/* forma de digitar tarjeta en el input*/
+document.getElementById("cardNumber").addEventListener('keyup', (e) => {
+ let valueCard = e.target.value; //guardando el valor del input, con target podemos acceder al valor
+ 
+//  borrando(reemp) letras u otra cosa e nuestro input
+ document.getElementById("cardNumber").value = valueCard.
+ //eliminando espacio en blanco \s significa space
+ replace(/\D/g, "") //todos los dígitos que no sean números 
+  
+ .replace(/\s/g, "") //reemp espacio por "nada"
+ .replace(/([0-9]{4})/g,"$1 ") //busca los número del 0 al 9 y los agrupa de 4 en 4 el dólar con 1 permite que en el último dígito haga un espacio
+
+//  .replace(/([0-9])/g,"*")
+
+ .trimEnd();//elimina el último espaciado
+
+ const hideDigits = validator.maskify("creditCardNumber");
+
+ showNumberInCard.textContent = valueCard;
+
+ if(valueCard === ''){
+     alert("Ingresa número de tarjeta");
+     showNumberInCard.textContent = " #### #### #### #### ";
+ }
+});
+
+
+
+//Mostrando nombre del titutlar
+document.getElementById("ownerName").addEventListener("keyup", (e) =>{
+    let valueName = e.target.value;
+
+    // eslint-disable-next-line no-undef
+    ownerName.value = valueName.replace(/[0-9]/g,"");
+    showNameInCard.textContent = valueName;
+
+    if(valueName === ''){
+        alert("Ingresa nombre del titular");
+        showNameInCard.textContent = "Lorem Ipsum";
+    }
+})
+
+// Mostrando selects mes y año
+
+const monthExpiration = document.querySelector(".monthCard");
+const yearExpiration = document.querySelector(".yearCard");
+
+document.getElementById("month").addEventListener('change', (e) => {
+    monthExpiration.textContent = e.target.value;
+});
+
+document.getElementById("year").addEventListener('change', (e) => {
+    yearExpiration.textContent = e.target.value;
+});
+
+
+
+// eslint-disable-next-line no-console
+// console.log(validator);
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,34 @@
+/*   
 const validator = {
-  // ...
-};
+    isValid : (creditCardNumber) => {
+        const creditCard = creditCardNumber.split('').reverse(); //cambio mi string por un array usando split y reversa para voltear el número
+        for(let i = 0 ; creditCard.length; i++) {
+            if ( i % 2 == 1) {
+                creditCard[i] = creditCard[i]*2;
+            }
+        }
+    }
 
-export default validator;
+    let creditCardDigits =  creditCard.join('').split(''),
+    let cardNumber = 0;
+    for(let i = 0 ; creditCardDigits.length; i++ ) {
+        cardNumber += parseInt(creditCardDigits[i]);
+    }
+     if (cardNumber % 10 == 0) {
+         return true;
+     } else {
+         return false;
+     } */
+
+/*     //para solo mostrar los últimos 4 dígitos
+    maskify : (creditCardNumber) =>  {
+        const cardDigits = creditCardNumber.split(''); //cambimos el string a array 
+        for(let i=0; i<cardDigits.length - 4; i++){//recorremos con el for menos los últimos 4 dígitos{
+         cardDigits[i]="*"; //reemp con *
+          }
+          //retornamos el string uniendo los últimso 4 elementos
+          return cardDigits.join('')
+    }
+  };
+  
+  export default validator; */
