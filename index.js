4b46c60865393e9b45b72624c4264ab4d1b9995f
VillalobosNicole <nicolevillalobosbe@gmail.com>
2021-01-26 12:32:37 -0500

    Updates

---
+++ index.js
@@ -0,0 +1,122 @@
+
+ import validator from './validator.js';
+ //import Swal from 'sweetalert2';
+ //const Swal = require('sweetalert2');
+ 
+
+ //VISUALIZACION DE PÁGINAS 
+ document.getElementById("firstPage").style.display = "block";
+ document.getElementById("card").style.display = "block";
+ document.getElementById("secondPage").style.display = "none";
+ document.getElementById("footerProject").style.display = "none";
+ let dataNumber = ''; //donde va a esatr el número de tarjeta sin los numerales 
+ const showNumberInCard = document.querySelector(" .number");
+ const showNameInCard = document.querySelector(" .titularName");
+ //QUE SE MUESTRE SOLO LA SEGUNDA PÁGINA
+ // eslint-disable-next-line no-unused-vars
+ document.getElementById("btnValidate").addEventListener('click', function(){
+  
+     if(dataNumber.length != 16 || document.getElementById("ownerName").value == "" || document.getElementById("month").value == "-" || document.getElementById("year").value == "-"  ){
+        // eslint-disable-next-line no-undef
+        Swal.fire({
+            icon: 'error',
+            title: 'Ups...',
+            text: 'Ingrese todos los datos solicitados '
+          })
+
+          
+      }else {
+
+     
+
+     document.getElementById("secondPage").style.display = "block";
+     document.getElementById("firstPage").style.display = "none";
+     document.getElementById("card").style.display = "none";
+     document.getElementById("footerProject").style.display = "block";
+     // eslint-disable-next-line no-console
+    // console.log('Calcular :', dataNumber);
+   let validate = validator.isValid(dataNumber);
+   // console.log('is Lumen : ', validate)
+     if(validate){
+       document.getElementById('answer').innerText = 'Tarjeta válida';
+     }else {
+       document.getElementById('answer').innerText = 'Tarjeta inválida';
+     }
+
+    }
+ 
+ });
+
+ 
+ 
+ const reload = document.getElementById('reload');
+ reload.addEventListener('click', function(){ 
+     location.reload();
+ }); // botón de retorno
+  
+
+ 
+  /* forma de digitar tarjeta en el input*/
+ document.getElementById("cardNumber").addEventListener('keyup', (e) => {
+     
+    //console.log(e , dataNumber);
+     if(dataNumber.length == 16 && e.key != "Backspace" ){
+
+        document.getElementById("cardNumber").value = validator.maskify(dataNumber); // input 
+        
+    }else {
+
+   // console.log(e);
+
+    if( !Number.isNaN ( Number(e.key) ) ){
+        //Adiciono
+        if ( e.key != ' '){
+            dataNumber += e.key;
+        }
+        
+    } else if( e.key == 'Backspace') {
+       dataNumber =  dataNumber.substring(0,dataNumber.length-1);
+    }
+    //console.log(dataNumber);
+    let value = validator.maskify(dataNumber);
+    //console.log(value);
+
+    showNumberInCard.textContent= value;
+    document.getElementById("cardNumber").value = value;
+    
+
+    }
+
+ if( dataNumber === ''){
+     //alert("Ingresa número de tarjeta");
+     showNumberInCard.textContent = " #### #### #### #### ";
+ } 
+});
+
+
+
+// //Mostrando nombre del titutlar
+document.getElementById("ownerName").addEventListener("keyup", (e) =>{
+    let valueName = e.target.value;
+
+    // eslint-disable-next-line no-undef
+    document.getElementById("ownerName").value = valueName.replace(/[0-9]/g,"");
+    showNameInCard.textContent = valueName;
+})
+
+// Mostrando selects mes y año
+
+const monthExpiration = document.querySelector(".monthCard");
+const yearExpiration = document.querySelector(".yearCard");
+
+document.getElementById("month").addEventListener('change', (e) => {
+    monthExpiration.textContent = e.target.value;
+});
+
+document.getElementById("year").addEventListener('change', (e) => {
+    yearExpiration.textContent = e.target.value;
+});
+
+
+// eslint-disable-next-line no-console
+// console.log(validator);
---
+++ validator.js
@@ -0,0 +1,58 @@
+   
+const validator = {
+     isValid : (creditCardNumber) => {
+        const creditCard = creditCardNumber.split('').reverse(); //hacemos un array y lo revertimos
+       // console.log('Reverse : ', creditCard);
+        let sumPairs = 0; //summa de pares
+        let sumImpairs =0; //suma de impares
+       /*  let p = [];
+        let im = []; */
+        let count = 0; // el contador inicializa en 0
+        for(let i= 0; i<(creditCard.length); i++){//ingresa al bucle for para dar el total de dígitos del string
+            let number = parseInt(creditCard[i]); //parseamos para que sea number
+            count ++;
+            if(count == 2){ //si el contador es par ingresa
+                let multiply = number*2;
+                if(multiply>=10){ //ingresa si al multiplicar el resultado es mayor igual a 10
+                   let digit =  multiply + ""; //lo convierto a string para tomar sus posiciones
+                   sumPairs+= parseInt(digit[0]) + parseInt(digit[1]); // parseo para que sea number
+
+
+                }else {
+                    sumPairs+= number*2; //ingresa si al multiplicar es menor a 10
+                }
+               
+                 count = 0; 
+                // p.push(number)
+            }else {
+                sumImpairs += number; //suma todos los impares
+                // im.push(number)
+            }
+        }
+
+        // console.log('Par : ', p);
+        // console.log('Impar : ', im);
+        //console.log(sumPairs, sumImpairs);
+        let totalSum = (sumPairs + sumImpairs) % 10; // sumo todos los números y veo si son divisibles por 10
+
+        if(totalSum === 0){ 
+            return true;
+        }else {
+            return false;
+        }
+
+       // return  ((totalSum % 10) === 0)? true : false; operador ternario 
+
+   }, 
+
+   //para solo mostrar los últimos 4 dígitos
+    maskify : (creditCardNumber) =>  {
+        const cardDigits = creditCardNumber.split(''); //cambimos el string a array 
+        for(let i=0; i<cardDigits.length - 4; i++){//recorremos con el for menos los últimos 4 dígitos{
+         cardDigits[i]="#"; //reemp con *
+          }
+          //retornamos el string uniendo los últimos 4 elementos
+          return cardDigits.join('')  //.replace(/([#0-9]{4})/g,"$1 ").trimEnd()
+    }
+}
+  export default validator; 
